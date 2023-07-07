aeb0a1addd31209751b75577eb82c4014bd6fd01
gopal-codes <gopalraut9840285859@gmail.com>
2023-07-07 11:23:18 +0545

    error solved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from '@/components/Footer'
+import Footer from '@/components/footer/Footer'
 import Nav from '../components/Nav'
 import './globals.css'
 import { Inter } from 'next/font/google'
