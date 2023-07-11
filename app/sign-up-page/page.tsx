450fa1eff35b3a2929a52d174dc83feb4ece18cd
Upama <upamadolma@gmail.com>
2023-07-11 17:15:32 +0545

    gopal

--- app/sign-up-page/page.tsx
+++ app/sign-up-page/page.tsx
@@ -1,71 +1,66 @@
 "use client";
 
-import Signup from "@/components/Signup";
-import SignupPro from "@/components/SignupPro";
 import Link from "next/link";
 import React from "react";
 import { FaUserPlus } from "react-icons/fa";
 
 const page = () => {
   return (
-    // <div className="flex flex-col  items-center gap-16 py-10">
-    //   <Link href="signup">
-    //     <div className="flex flex-col items-center">
-    //       <img
-    //         className="w-[100px] md:w-[150px]"
-    //         src="../Assets/ts-final-logo.png"
-    //         alt="logo"
-    //       />
+    <div className="flex flex-col  items-center gap-16 py-10">
+      <Link href="signup">
+        <div className="flex flex-col items-center">
+          <img
+            className="w-[100px] md:w-[150px]"
+            src="../Assets/ts-final-logo.png"
+            alt="logo"
+          />
 
-    //       <p className="text-[13px] text-[#747774] leading-9">
-    //         Sign Up on Technical Sewa
-    //       </p>
-    //     </div>
-    //   </Link>
-    //   <div className="flex flex-col md:flex-row  gap-10 ">
-    //     {/* sign up to hire  */}
-    //     <div className="flex flex-col items-center gap-4 text-center">
-    //       <FaUserPlus
-    //         className="border-[2px] border-[#f87171] rounded-full p-2"
-    //         size="3.6rem"
-    //         color="#2591b2"
-    //       />
+          <p className="text-[13px] text-[#747774] leading-9">
+            Sign Up on Technical Sewa
+          </p>
+        </div>
+      </Link>
+      <div className="flex flex-col md:flex-row  gap-10 ">
+        {/* sign up to hire  */}
+        <Link
+          href="/sign-up-page/sign-up"
+          className="flex flex-col items-center gap-4 text-center "
+        >
+          <FaUserPlus
+            className="border-[2px] border-[#f87171] rounded-full p-2 "
+            size="3.6rem"
+            color="#2591b2"
+          />
 
-    //       <span className="text-[20px] text-[#a6adbb]">Sign Up to Hire</span>
-    //       <p className="text-[13px] text-[#a6adbb]">
-    //         Hire the right Professional for your project
-    //       </p>
-    //     </div>
+          <span className="text-[20px] text-[#a6adbb]">Sign Up to Hire</span>
+          <p className="text-[13px] text-[#a6adbb]">
+            Hire the right Professional for your project
+          </p>
+        </Link>
 
-    //     {/* sign up as a professinal  */}
-    //     <div className="flex flex-col items-center gap-4 text-center">
-    //       <FaUserPlus
-    //         className="border-[2px] border-[#f87171] rounded-full p-2"
-    //         size="3.6rem"
-    //         color="#2591b2"
-    //       />
-    //       <span className="text-[20px] text-[#a6adbb]">
-    //         Sign Up as a Professional
-    //       </span>
-    //       <p className="text-[13px] text-[#a6adbb]">
-    //         Expand your service business with <br />{" "}
-    //         <span className="font-bold">Technical Sewa</span>
-    //       </p>
-    //     </div>
-    //   </div>
-    // </div>
-
-    // sign up page started..
-    <div>
-      <Signup />
+        {/* sign up as a professinal  */}
+        <Link
+          href="/sign-up-page/sign-up-pro"
+          className="flex flex-col items-center gap-4 text-center"
+        >
+          <FaUserPlus
+            className="border-[2px] border-[#f87171] rounded-full p-2 "
+            size="3.6rem"
+            color="#2591b2"
+          />
+          <span className="text-[20px] text-[#a6adbb]">
+            Sign Up as a Professional
+          </span>
+          <p className="text-[13px] text-[#a6adbb]">
+            Expand your service business with <br />
+            <span className="font-bold">Technical Sewa</span>
+          </p>
+        </Link>
+      </div>
     </div>
 
     // sign up page enededskjflks
     // sign up pro started...
-
-    // <div>
-    //   <SignupPro />
-    // </div>
   );
 };
 
--- app/sign-up-page/sign-up/page.tsx
+++ app/sign-up-page/sign-up/page.tsx
@@ -1,12 +1,58 @@
+"use client";
+
 import React, { useState } from "react";
 import { BsEyeSlashFill } from "react-icons/bs";
 import { AiOutlineEye } from "react-icons/ai";
+import axios from "axios";
+import { baseUrl } from "@/app/otherItem/baseUrl";
 
-const Signup = () => {
+const page = () => {
+  const [input, setInput] = useState({
+    mobilenumber: "",
+    firstname: "",
+    lastname: "",
+    emailaddress: "",
+    address: "",
+    password: "",
+    confirmpassword: "",
+    refferedby: "",
+  });
+
+  let data = new FormData();
+
+  console.log(data);
+
+  const handleSignUp = async () => {
+    if (input.password === input.confirmpassword) {
+      data.append("phone", input.mobilenumber);
+      data.append("first_name", input.firstname);
+      data.append("last_name", input.lastname);
+      data.append("email", input.emailaddress);
+      data.append("address", input.address);
+      data.append("password", input.password);
+      data.append("confirm_password", input.confirmpassword);
+      data.append("reffered_by", input.refferedby);
+
+      await axios
+        .post(`${baseUrl}techsewa/masterConfig/publicLogin/Signup`, data)
+        .then((response) => {
+          console.log(response.data.message);
+        });
+    } else {
+      alert("Password and confirm password do not match");
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+
   const [showPassword, setshowPassword] = useState(false);
   const [showConfirmPassword, setshowConfirmPassword] = useState(false);
 
   return (
+    // sign up page started..
     <div className="flex flex-col  items-center gap-10 py-10">
       {/* logo and text  */}
       <div className="flex flex-col items-center">
@@ -24,10 +70,12 @@ const Signup = () => {
       {/* input  */}
       <div className="w-[80%] lg:w-[33.33%]  flex flex-col gap-5">
         <input
+          minLength={10}
           name="mobilenumber"
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="Mobile Number"
+          onChange={handleChange}
           required
         />
         <input
@@ -35,6 +83,7 @@ const Signup = () => {
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="First Name"
+          onChange={handleChange}
           required
         />
         <input
@@ -42,6 +91,7 @@ const Signup = () => {
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="Last Name"
+          onChange={handleChange}
           required
         />
         <input
@@ -49,6 +99,7 @@ const Signup = () => {
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="Email Address"
+          onChange={handleChange}
           required
         />
         <input
@@ -56,20 +107,21 @@ const Signup = () => {
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="Address"
+          onChange={handleChange}
           required
         />
 
         {/* password  */}
         <div className="flex items-center  border-[1px] justify-between">
-          <div className=" pl-4">
-            <input
-              name="password"
-              className=" text-[14px] italic outline-none"
-              type={`${showPassword === false ? "password" : "text"}`}
-              placeholder="Password"
-              required
-            />
-          </div>
+          <input
+            name="password"
+            className=" w-full py-[12px] pl-[20px]   placeholder:italic placeholder:font-normal rounded-[2px] outline-none text-[14px]"
+            type={`${showPassword === false ? "password" : "text"}`}
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+
           <div
             className=" border-l-[1px] p-4"
             onClick={() => setshowPassword(!showPassword)}
@@ -78,15 +130,15 @@ const Signup = () => {
           </div>
         </div>
         <div className="flex items-center  border-[1px] justify-between">
-          <div className=" pl-4">
-            <input
-              name="confirmpassword"
-              className=" text-[14px] italic outline-none"
-              type={`${showConfirmPassword === false ? "password" : "text"}`}
-              placeholder="Confirm Password"
-              required
-            />
-          </div>
+          <input
+            name="confirmpassword"
+            className=" w-full py-[12px] pl-[20px]   placeholder:italic placeholder:font-normal rounded-[2px] outline-none text-[14px]"
+            type={`${showConfirmPassword === false ? "password" : "text"}`}
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            required
+          />
+
           <div
             className=" border-l-[1px] p-4"
             onClick={() => setshowConfirmPassword(!showConfirmPassword)}
@@ -100,10 +152,14 @@ const Signup = () => {
           className=" w-full  p-4 border-[1px] text-[14px] italic outline-none"
           type="text"
           placeholder="Reffered By"
+          onChange={handleChange}
           required
         />
 
-        <button className="w-full bg-[#2591b2] p-4 text-white rounded-md">
+        <button
+          className="w-full bg-[#2591b2] p-4 text-white rounded-md"
+          onClick={handleSignUp}
+        >
           Submit
         </button>
       </div>
@@ -111,4 +167,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default page;
