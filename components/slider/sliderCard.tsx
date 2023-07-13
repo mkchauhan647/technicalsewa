b9ad6ae65323615d1500ee372f033e5c39ec868d
gopal-codes <gopalraut9840285859@gmail.com>
2023-07-13 10:13:53 +0545

    gopal

--- components/slider/sliderCard.tsx
+++ components/slider/sliderCard.tsx
@@ -1,30 +1,29 @@
 "use client"
-
-import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
-import Slider from "./Slider";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const SliderCard = ({data}:any) => {
-
+const SliderCard = ({length,data}:any) => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+    slidesToShow: length,
     slidesToScroll: 1,
-    autoplay:true
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
   };
 
-
   return (
-    <Slider {...settings} className="overflow-hidden flex justify-center items-center gap-2 cursor-pointer">
-      {data?.map((ele: any) => (
-        <Link href="/service/lcd-led-tv" className="w-1/6" >
-        <div key={ele?.title} >
-          <img width={10} height={10} src={ele?.image_url? ele?.image_url:""} alt={ele?.alt} className="h-[100px] w-[150px]" />
-        </div>
+    <Slider {...settings} className="overflow-hidden flex justify-center items-center gap-1 cursor-pointer">
+      {data?.map((ele: any, index:any) => (
+        <div key={index}  className={``}>
+        <Link  href={"service" + "/" + ele.product_name } className={``} >
+          <img  src={ele?.image_url? ele?.image_url:""} alt={ele?.alt2} className="h-[100px] w-[150px]" />
         </Link>
+        </div>
       ))}
     </Slider>
   );
