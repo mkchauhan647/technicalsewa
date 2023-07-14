e88301c11a0120294b02ab036725f6da4b7e838d
gopal-codes <gopalraut9840285859@gmail.com>
2023-07-14 13:24:42 +0545

    singlepage made by sachin

--- components/slider/SlideCard.tsx
+++ components/slider/SlideCard.tsx
@@ -12,9 +12,39 @@ const SlideCard = ({ length, data }: any) => {
         slidesToShow: length,
         slidesToScroll: 1,
         autoplay: true,
-        speed: 2000,
-        autoplaySpeed: 2000,
+        speed: 3000,
+        autoplaySpeed: 3000,
         cssEase: "linear",
+        responsive: [
+          {
+          breakpoint: 1024,
+          settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+          }
+          },
+          {
+          breakpoint: 600,
+          settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          speed: 10000,
+          autoplaySpeed: 10000,
+          initialSlide: 1
+          }
+          },
+          {
+          breakpoint: 480,
+          settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          speed: 10000,
+          autoplaySpeed: 10000,
+          }
+          }
+          ]
       };
     
       return (
@@ -31,7 +61,7 @@ const SlideCard = ({ length, data }: any) => {
                 <img
                   src={ele?.image_url ? ele?.image_url : ""}
                   alt={ele?.alt2}
-                  className="bg-white h-[100px] w-[160px] p-4 border-[1px] border-gray-100 rounded-sm hover:scale-[1.1] transition-hover duration-300"
+                  className="bg-white h-[100px] w-[160px] border-[1px] border-gray-100 rounded-sm hover:scale-[1.1] transition-hover duration-300"
                 />
                 <h1 className="text-[16px] text-center whitespace-nowrap font-bold">
                   {ele?.product_name}
