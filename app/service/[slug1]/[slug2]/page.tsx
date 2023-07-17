bd36ceee7879fbef4c9ec0823a89b1e43baa80a6
gopal-codes <gopalraut9840285859@gmail.com>
2023-07-18 03:51:21 +0545

    slider ui, single description, and using server component for api call

--- app/service/[slug1]/[slug2]/page.tsx
+++ app/service/[slug1]/[slug2]/page.tsx
@@ -1,13 +1,18 @@
 import Nav from '@/components/Nav'
 import Footer from '@/components/footer/Footer'
 import ServiceSlug2 from '@/components/pageHelperComponents.js/ServiceSlug2'
+import { baseUrl } from '@/public/baseUrl';
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+
+  const result = await fetch(`${baseUrl}multiservice/masterconfig/publicmasterconfig/getSliderListpop`);
+  const data = await result.json();
+
   return (
     <>
     <Nav />
-    <ServiceSlug2 />
+    <ServiceSlug2 data= {data.brands} />
     <Footer />
     </>
   )
