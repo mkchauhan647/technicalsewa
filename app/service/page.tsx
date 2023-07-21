2976e21dc7811689cad5957cb5026ad889f1ed20
gopal-codes <gopalraut9840285859@gmail.com>
2023-07-21 13:25:32 +0545

    slider updated

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,25 +1,13 @@
 import Nav from '@/components/Nav'
 import Footer from '@/components/footer/Footer'
 import Service from '@/components/pageHelperComponents.js/Service'
-import { baseUrl } from '@/public/baseUrl'
-import axios from 'axios'
 import React from 'react'
 
-const page = async() => {
-
-  const result = await axios.get(`${baseUrl}multiservice/masterconfig/publicmasterconfig/getSliderListpop1`);
-  const data = result?.data?.brands;
-
-    // Data for Popular Brands that is placed at rigth bottom of service page
-    const PopularBrands = data?.filter((val: any) => {
-      return val.brand_name === "Popular Brands";
-    });
-  
-
+const page = () => {
   return (
     <>
     <Nav />
-    <Service data={data} PopularBrands={PopularBrands} />
+    <Service />
     <Footer />
     </>
   )
