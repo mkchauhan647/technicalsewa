8ead72786f8913cc24c90d49582f11ef0bea7152
Shambhu Prasad Chaudhary <shambhuchu21@gmail.com>
2023-10-05 16:04:31 +0545

    changes in category page

--- components/repair/Categories.tsx
+++ components/repair/Categories.tsx
@@ -30,7 +30,7 @@ const Categories = async ({ allBrands }: any) => {
 
   return (
     <div className="pt-[40px] pb-[40px]  flex justify-around md:justify-center items-center flex-wrap  ">
-      <div className="lg:!container md:w-[1280px] flex justify-around items-center flex-wrap gap-5 md:gap-4 ">
+      <div className="lg:!container md:max-w-[1140px] mx-auto flex justify-around items-center flex-wrap gap-5 md:gap-4 ">
         {data?.brands?.map((category: any) => (
           <CategoryCard
             id={category.brand_id}
