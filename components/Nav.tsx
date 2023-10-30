a26595917dc6a27dc664b11a529def92aa98f54e
Shambhu Prasad Chaudhary <shambhuchu21@gmail.com>
2023-10-30 17:52:29 +0545

    chnages in navabr of blogs and training

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -116,7 +116,7 @@ const Nav = () => {
               {Array.isArray(categories) && categories.length && (
                 <div className="hidden group-hover:block">
                   <div className="absolute z-10 mt-0 bg-white rounded-md shadow-lg md:w-[350px]">
-                    <div className="py-1 h-[400px] overflow-y-scroll ">
+                    <div className="py-1 h-[360px] overflow-y-scroll ">
                       <div className="py-2 pt-1"></div>
                       {categories.map((cat, i) => {
                         const slug = cat?.text?.replace(" ", "-").toLowerCase();
@@ -126,7 +126,7 @@ const Nav = () => {
                             href={`/training/${slug}`}
                             className="w-[full]"
                           >
-                            <p className="block px-6 py-2 text-sm text-[grey] hover:bg-gray-100">
+                            <p className="block px-6 py-0.5 text-sm text-[grey] hover:bg-gray-100">
                               {cat.text}
                             </p>
                             <hr />
