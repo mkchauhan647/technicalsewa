3f66bca4062678bc720f87fac0f8a6aa3bdd56e0
mkchauhan647 <mkchauhan647@gmail.com>
2024-09-06 13:15:58 +0545

    blog category url update...

--- components/BlogCategorylist.tsx
+++ components/BlogCategorylist.tsx
@@ -11,10 +11,16 @@ const BlogCategorylist = ({ categories = [] }: { categories: string[] }) => {
     <div>
       {categories.map((cat: any, index: number) => (
         <Link
+          
           // onClick={() => handleCatClick(cat?.value)}
+          // href={`/blogs/${cat.text
+          //   ?.replaceAll(" ", "-")
+          //   .toLowerCase()}
+          //   /${cat?.value}`}
           href={`/blogs/${cat?.text
-            ?.replaceAll(" ", "-")
-            .toLowerCase()}/${cat?.value}`}
+            ?.toLowerCase().trim()
+            .replace(/[^a-zA-Z0-9-\s]/g, "") // Replace special characters with -
+            .replace(/\s+/g, "-")}`}
           key={index}
           className="cursor-pointer hover:text-primary hover:font-semibold flex  items-center justify-between py-1 border-b-[1px] border-solid border-[#3d4145]"
         >
