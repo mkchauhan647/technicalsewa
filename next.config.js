15fa7ae32cd2f34c6c293172df72ec1d571b8be7
Manoj Kumar Chauhan <44749661+mkchauhan647@users.noreply.github.com>
2025-01-11 11:59:43 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     domains: ["www.technicalsewa.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://crm.technicalsewa.com/api/:path*", // Proxy to Backend
+      },
+      {
+        source: "/spareparts/:path*", // Proxy requests to the spare parts site
+        destination: "https://technicalsewa-ecommerce.vercel.app/spareparts/:path*",
+      },
+    ];
+  },
+
 };
 
 module.exports = nextConfig;
